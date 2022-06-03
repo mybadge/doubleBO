f02d446330fb97f3cc22c22b7e2f6b4e98d82b80
zhaozhidan <zhaozhidan@baijiayun.com>
2022-06-03 12:26:27 +0800

    fix: ---

--- pack.sh
+++ pack.sh
@@ -1,8 +1,3 @@
 #! /bin/bash
 yarn build
-mv build db
-zip -r zip.zip db
-scp -r zip.zip root@148.70.218.192:/var/www/static/
-
-mv db build
-rm -rf zip.zip
+scp -rf build/* root@148.70.218.192:/var/www/static/db
