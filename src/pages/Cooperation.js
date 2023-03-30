cc3a25a3288576324fc3e069dd42cfdc501c83e7
zhaozhidan <zhaozhidan@baijiayun.com>
2023-03-30 20:13:09 +0800

    feat:加载线上图片资源

--- src/pages/Cooperation.js
+++ src/pages/Cooperation.js
@@ -17,7 +17,7 @@ export default class About extends PureComponent {
             <li>依托中粮强大资源及品牌优势，是确保合作伙伴获取丰厚利益强有力的保障。</li><li>我们拥有中粮提供的期限为20年的优质、免费的生产生态沼液肥的原材料及建厂用地和水电设施，这些优质资源及中粮强大的品牌效应，对合作者来说，是一个绝佳的投入机会，未来必将获得巨大的利益。</li><li>选择与我公司合作，不但能帮您获取超出想象的财富结果，而且还能彰显您的人生价值，甚至改变您整个家族的命运。</li><li>公司拥有已获授权的16项实用新型和三项发明专利。</li>
           </ul>
 
-          <Title level={2} style={{ textAlign: 'left' }}>二、合作方式</Title>
+          {/* <Title level={2} style={{ textAlign: 'left' }}>二、合作方式</Title>
           <Title level={4} style={{ textAlign: 'left' }}>1、联合创始人合作方式（期限为5年）</Title>
           <ul>
             <li>
@@ -43,14 +43,14 @@ export default class About extends PureComponent {
             <li>三级代理：一次定购5吨产品（3万）；享有每吨返润500元可累计升级一至二级代理；合同有效期内，如代理商在代理合同期满时未将订购的产品销售出去，且不在续签代理合同，公司一次性将代理商未提走部分产品原价退给代理商，代理商亦不享有配送股份。
             </li></ul>
           <Title level={4} style={{ textAlign: 'left' }}>4、推荐人合作方式</Title>
-          <ul><li>推荐人每成功为公司推荐一位合作伙伴，即获得投资伙伴已实际支付投资金额10%的奖励。励（代金券+现金，代金券在重复消费时可以抵现金）；</li></ul>
-          <Title level={2} style={{ textAlign: 'left' }}>三、欢迎加入</Title>
+          <ul><li>推荐人每成功为公司推荐一位合作伙伴，即获得投资伙伴已实际支付投资金额10%的奖励。励（代金券+现金，代金券在重复消费时可以抵现金）；</li></ul> */}
+          <Title level={2} style={{ textAlign: 'left' }}> 二、欢迎加入</Title>
           <Paragraph>因您的要求而改变财富电话：022-22458698、15030694688</Paragraph>
           <Paragraph>
             单位地址：天津市宝坻区史各庄镇尚庄商业楼203-204室
           </Paragraph>
         </Typography>
-      </div>
+      </div >
     );
   }
 }
